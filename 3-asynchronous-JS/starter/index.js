03dfcc5fab15ef73e71389727ca155cd83ffd746
Uzzul <uzzul.hussain@adp.com>
2023-10-04 09:44:40 -0400

    first commit

---
+++ 3-asynchronous-JS/starter/index.js
@@ -0,0 +1,52 @@
+const { rejects } = require("assert");
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("Error, file not found!!!");
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Error, try again");
+      resolve("successfully updated file");
+    });
+  });
+};
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    console.log(await writeFilePro("dog-img.txt", res.body.message));
+  } catch (err) {
+    console.log(err);
+  }
+};
+getDogPic();
+// readFilePro
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     return writeFilePro("dog-img.txt", res.body.message);
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
